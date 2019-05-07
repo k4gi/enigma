ad2a0e178f0ea33ce730788a44fb9c24fa5c3ceb
k4gi <k4gi@pm.me>
2019-05-07 14:37:37 +1000

    cleaned up comments in java version, moved some to the readme

--- Cipher3.java
+++ Cipher3.java
@@ -50,17 +50,6 @@ public class Cipher extends JFrame {
 		in = in.toUpperCase();
 		String out = "";
 		String keyboard = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		//String plugboard = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //default! not messing with this rn
-		
-		/*
-			Gotta say a little bit about the stepping mechanism.
-			Rotors I-V  have one step, rotors VI-VIII have two.
-			The thing is they're written down as the place on the entry character
-			so I guess I do need to keep track of both after all
-			come to think of it why did I think I didn't? Of course I need to know where its turned to!
-			Anyway. Yes.
-			I'm just going to write them down right now as A-B, meaning when A clicks to B
-		*/
 		String[] wheel_i = { // Q-R
 			"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 			"EKMFLGDQVZNTOWYHXUSPAIBRCJ",
@@ -93,49 +82,9 @@ public class Cipher extends JFrame {
 			"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 			"FKQHTLXOCBJSPDZRAMEWNIUYGV",
 			"AN"};
-		//reflectors don't turn so i guess i can just compare them to the keyboard	
 		String reflector_b = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
 		String reflector_c = "FVPJIAOYEDRZXWGCTKUQSBNMHL";
-		// these wheels copied from http://users.telenet.be/d.rijmenants/en/enigmatech.htm
 		
-		/*
-			Here is a video of a paper enigma machine in action.
-			https://www.youtube.com/watch?v=pZsuxZXN33g
-			Notice that the wheels, rotors, whatever, move BEFORE encoding the letter typed!
-			
-			The Enigma machine works by sending an electrical signal...
-			From the keyboard, to the plugboard, to a static wheel that connects to the moving wheels...
-			From there to wheel III, to wheel II, to wheel I, to the reflector, then back again...
-			Wheel I, II, III, static wheel, plugboard, then the lampboard. Result!
-			Oh right and in some versions there are four wheels!
-			
-			ugh i'm going to write that out again.
-			1. Input character
-			2. Shift rotors (or wheels) (do i have to pick one?)
-			3. Plugboard
-			4. Wheel III
-			5. Wheel II
-			6. Wheel I
-			7. Reflector
-			8. Wheel I
-			9. Wheel II
-			10. Wheel III
-			11. Plugboard
-			12. Output character!!!!!!!
-			
-			Wheel specs are (apparently) recorded here:
-			https://en.wikipedia.org/wiki/Enigma_rotor_details
-			http://users.telenet.be/d.rijmenants/en/enigmatech.htm
-			https://www.codesandciphers.org.uk/enigma/rotorspec.htm
-			The wikipedia page has more rotors than the others
-		*/
-		
-		//I'm realising that I should have written more comments in the other Cipher files...
-		//I don't know what's happening! I need to read it carefully...again!
-		
-		//I'm just going to go for it
-		
-		//Step 1: get input (that's already done yep)
 		debug("Input: " + in);
 		debug("Slot 1: " + in_wheel1);
 		debug("Setting 1: " + in_start1);
@@ -143,36 +92,12 @@ public class Cipher extends JFrame {
 		debug("Setting 2: " + in_start2);
 		debug("Slot 3: " + in_wheel3);
 		debug("Setting 3: " + in_start3);
-		// It's March 15 and I've just got some settings passing through to this function.
-		// It's time to make them actually set some things.
-		// I've got Strings to tell me which wheels to use in each of the three slots,
-		// And I've got ints to tell me what their settings should be.
-		// Hey, are the roll-over points still hard-coded just down there?
-		// No it looks like I've dealt with that already.
-
-
-		//Step 2: shift rotorwheel III, this code from Cipher2.java, line 58 is what I need...
-		//wheel_2 = wheel_2.substring(1, wheel_2.length()) + wheel_2.charAt(0);
-		//oh I need like 'working directory' wheels
-		//String[] working_wheel_i = wheel_i;
-		//String[] working_wheel_ii = wheel_ii;
-		//String[] working_wheel_iii = wheel_iii; //the numbers, match, yes, just because
+		
 		String[] working_wheel_i;
 		String[] working_wheel_ii;
 		String[] working_wheel_iii;
 		String working_reflector;
-		/*
-			There's a bug right now to do with these wheel...
-			uh I had the word just now, declarations or something
-			but anyway if two or more working_wheels are the same wheel,
-			then when one gets turned, they all do.
-			I could just stop the same wheel being selected twice,
-			since thats not something you do with an enigma machine,
-			you only have one copy of each wheel.
-			But it'd be better to sort out this problem instead I think.
-
-			OK fixed :)
-		*/
+		
 		switch(in_wheel1) {
 			case "Wheel I" :
 				working_wheel_i = wheel_i.clone();
@@ -291,116 +216,41 @@ public class Cipher extends JFrame {
 		//for each character in the input string
 		for(int i = 0; i < in.length(); i++) {
 			debug("==========START OF LOOP==========");
-			
+
+			//Step 1: get input
 			char code_letter = in.charAt(i);
 			debug("code_letter: " + code_letter);
 			if(keyboard.indexOf(code_letter) == -1) { //skip character if it's not a letter, don't move wheels
 				continue;
 			}
-			
+
+			//Step 2: shift wheels
 			debug("Shifting rotors...");
-			//Yeah so clearly there's something wrong with how I'm moving the wheels
-			//Alphabet gets shifted, coded letters get shifted, that means it ends up being the same every time
-			//So I'm doing it wrong. Gotta figure out the solution.
-			//Good news, the code "works" at least!
 		
 			working_wheel_iii[0] = working_wheel_iii[0].substring(1, working_wheel_iii[0].length()) + working_wheel_iii[0].charAt(0);
 			working_wheel_iii[1] = working_wheel_iii[1].substring(1, working_wheel_iii[1].length()) + working_wheel_iii[1].charAt(0);
-			//i really hope i can just edit the strings like this
-			//time for checking if i need to do it again
-			if( working_wheel_iii[0].charAt(0) == working_wheel_iii[2].charAt(0) || working_wheel_iii[0].charAt(0) == working_wheel_iii[2].charAt(1) ) { //need to add the second tick for wheels VI, VII, VIII
+			if( working_wheel_iii[0].charAt(0) == working_wheel_iii[2].charAt(0) || working_wheel_iii[0].charAt(0) == working_wheel_iii[2].charAt(1) ) {
 				
 				working_wheel_ii[0] = working_wheel_ii[0].substring(1, working_wheel_ii[0].length()) + working_wheel_ii[0].charAt(0);
 				working_wheel_ii[1] = working_wheel_ii[1].substring(1, working_wheel_ii[1].length()) + working_wheel_ii[1].charAt(0);
 				
-				if( working_wheel_ii[0].charAt(0) == working_wheel_ii[2].charAt(0) || working_wheel_ii[0].charAt(0) == working_wheel_ii[2].charAt(1) ) { //need to add the second tick for wheels VI, VII, VIII
+				if( working_wheel_ii[0].charAt(0) == working_wheel_ii[2].charAt(0) || working_wheel_ii[0].charAt(0) == working_wheel_ii[2].charAt(1) ) {
 					
 					working_wheel_i[0] = working_wheel_i[0].substring(1, working_wheel_i[0].length()) + working_wheel_i[0].charAt(0);
 					working_wheel_i[1] = working_wheel_i[1].substring(1, working_wheel_i[1].length()) + working_wheel_i[1].charAt(0);
 					//since we're only doing three wheels (for now) we can stop here. The reflector doesn't move.
 				}
-			} //at this point it still compiles (though we're not using this code yet) so its probably fine right??
-			//this is the end of turning the wheels now i need to encode
-			//just a thought how the ufck am i going to test this code
+			}
 			
 			debug( "W-I:   " + working_wheel_i[0] + " / " + working_wheel_i[1] + " / " + working_wheel_i[2] );
 			debug( "W-II:  " + working_wheel_ii[0] + " / " + working_wheel_ii[1] + " / " + working_wheel_ii[2] );
 			debug( "W-III: " + working_wheel_iii[0] + " / " + working_wheel_iii[1] + " / " + working_wheel_iii[2] );
 			
-			//Step 3: plugboard.... ummm its first thing in the morning i'll come back later
-			//need to set up the main loop, so I can tell which character to be looking at, to go forward from here
+			//Step 3: plugboard
 			
 			code_letter = plugboard.charAt( keyboard.indexOf(code_letter) );
 			debug("Plugboard... " + code_letter);
 			
-			//just having some trouble here working out the code...
-			//i'm sure it'll come together soon but its confusing...
-			//found an Enigma project made with an arduino. Pretty cool.
-			// https://www.instructables.com/id/Make-your-own-Enigma-Replica/
-			//it's got the code there too so I can check that out when I'm done
-			
-			
-			/*
-				I think the problem I'm having is with the directionality of the wheels.
-				Not sure though. I've just gotta work it out, make sure all the wheels are connected the right way.
-				Each wheel has contacts that align with whatever position they're each at. It's pretty neat.
-				I don't even know what my two sets of characters on each wheel really mean. It's a bit abstract.
-				A becomes E? E becomes L? Z becomes J? Or is it the other way around? Does it matter? I guess it does.
-				The site I got the characters from has some diagrams that would probably be helpful.
-				http://users.telenet.be/d.rijmenants/en/enigmatech.htm
-			*/
-			
-			/*
-				OK it's most of the way through February and I'm back to finish this off
-				I've been mulling it over for a long while
-				And I think I have the solution.
-				Each wheel has two parts, right!
-				The left, which is the normal alphabet
-				And the  right, which is the scrambled alphabet.
-				The way I have the wheels represented right now is insufficient,
-				because it doesn't take into account the relative positions of,
-				the contacts of the left and right, and how they're wired to each other.
-				...
-				ugh, no, that's not right, it's fine how they're represented.
-				I just need to do some of the transformations backwards.
-				Or something. You know what maybe I should just have another go at it.
-				yeah here goes
-				
-				...
-				
-				its nearly the end of february and i'm back again trying to make this happen oh boy
-				beginning of march! look i'm getting there ok
-				
-				the steps to move forward (left) through the enigma machine are...
-				1. Identify the entry point
-					To make it easier I'm going to transform code_letter into its un-rotated form between each wheel
-					This is derived from the position of string[0] compared to keyboard
-					there's some charAt / indexOf calculations that can take care of that pretty easy
-					but they're not coming to mind right now but that's ok
-				2. Move through the wheel
-					Going forward, this is a simple case of matching the entry character in string [0] with its counterpart in [1]
-				3. Identify exit point
-					So you have the new letter from string[1]. But it needs to exit at the position of string[0]!
-					Tranform code_letter into its un-rotated form, based on that position.
-					
-				the steps to move backward (right) are...
-				1. Identify the entry point
-					This is the same as going forward. Transform the un-rotated letter with string[0]
-				2. Move through the wheel
-					Going through the wheel backwards has been fucking with me for so long but I really think I have it this time.
-					We just need to find code_letter in string[1], and transform it into the matching char in string [0]
-				3. Identify the exit point
-					Since we're going backwards we're already in the right position to un-rotate code_letter
-				
-				steps to move through the reflector...
-				1. transform code_letter
-					The reflector doesn't rotate so just find code_letter in keyboard and match the position to the reflector
-				2. exit reflector
-					you know what i don't think there's anything else to do here
-				
-				steps to move through the plugboard
-				1. do the same thing as for the reflector
-			*/
 			
 			
 			//Step 4: Wheel III
@@ -412,11 +262,6 @@ public class Cipher extends JFrame {
 			//exit
 			code_letter = keyboard.charAt( working_wheel_iii[0].indexOf(code_letter) );
 			
-			//code_letter = working_wheel_iii[1].charAt( keyboard.indexOf(code_letter) );
-			
-			//which contact am i touching?? keyboard.indexOf(code_letter)
-			//which letter (left side) is connected to that contact?? i don't know
-			//i need to know this
 			debug("Wheel III... " + code_letter);
 			
 			//Step 5: Wheel II
@@ -428,7 +273,6 @@ public class Cipher extends JFrame {
 			//exit
 			code_letter = keyboard.charAt( working_wheel_ii[0].indexOf(code_letter) );
 			
-			//code_letter = working_wheel_ii[1].charAt( working_wheel_iii[0].indexOf(code_letter) );
 			debug("Wheel II...  " + code_letter);
 			
 			//Step 6: Wheel I
@@ -440,14 +284,12 @@ public class Cipher extends JFrame {
 			//exit
 			code_letter = keyboard.charAt( working_wheel_i[0].indexOf(code_letter) );
 			
-			//code_letter = working_wheel_i[1].charAt( working_wheel_ii[0].indexOf(code_letter) );
 			debug("Wheel I...   " + code_letter);
 			
-			//Step 7: Reflector ... Let's just use reflector B for now
+			//Step 7: Reflector
 			
 			code_letter = working_reflector.charAt( keyboard.indexOf(code_letter) );
 			
-			//code_letter = reflector_b.charAt( working_wheel_i[0].indexOf(code_letter) );
 			debug("Reflector... " + code_letter);
 			
 			//Step 8: Wheel I
@@ -456,12 +298,9 @@ public class Cipher extends JFrame {
 			code_letter = working_wheel_i[0].charAt( keyboard.indexOf(code_letter) ); //ready to go through wheel
 			//encode (but backwards!}
 			code_letter = working_wheel_i[0].charAt( working_wheel_i[1].indexOf(code_letter) ); //passing backwards through wheel
-			//so how about it did i do it this time??? 2019-02-26
-			//yes i thignk i might have 2019-03-05
 			//exit
 			code_letter = keyboard.charAt( working_wheel_i[0].indexOf(code_letter) );
 			
-			//code_letter = working_wheel_i[1].charAt( working_wheel_ii[0].indexOf(code_letter) );
 			debug("Wheel I...   " + code_letter);
 			
 			//Step 9: Wheel II
@@ -473,7 +312,6 @@ public class Cipher extends JFrame {
 			//exit
 			code_letter = keyboard.charAt( working_wheel_ii[0].indexOf(code_letter) );
 			
-			//code_letter = working_wheel_ii[1].charAt( working_wheel_iii[0].indexOf(code_letter) );
 			debug("Wheel II...  " + code_letter);
 			
 			//Step 10: Wheel III
@@ -485,7 +323,6 @@ public class Cipher extends JFrame {
 			//exit
 			code_letter = keyboard.charAt( working_wheel_iii[0].indexOf(code_letter) );
 			
-			//code_letter = working_wheel_iii[1].charAt( keyboard.indexOf(code_letter) );
 			debug("Wheel III... " + code_letter);
 			
 			//Step 11: Plugboard
@@ -538,27 +375,6 @@ public class Cipher extends JFrame {
 			plugboard_menu[i] = new JComboBox();
 			plugboard_menu[i].addItem("---");
 		}
-		/*
-			March 16, the last thing to do to make this program fully functional is
-			to restrict plugboard settings to how the plugboard actually works.
-			That is to say, one letter to another, connected both ways.
-			I think the machine also only had ten plugs but do I need to stick to that?
-			Anyway yeah I'm thinking adding a ... changeListener() ?
-			To each of the 26 plugboardmenu[] JComboBox 's
-			To automatically set the corresponding letter to match
-			And to remove both letters from the lists of all others
-			Which seems hella messy. This whole program is getting hella messy.
-			But its working! That's the important thing. I can change it later
-			to make it more efficient and less ridiculous later, yep.
-
-			Oh, I suppose the other way to go would be NOT restricting the UI
-			and just telling off the user if the plugboard they're about to use
-			is invalid. That'd be easier, but I'd really like a proper UI,
-			but this JComboBox nonsense clearly isn't the best way to go anyway.
-
-			Then again I'm not really sure how to go about checking the
-			plugboard after the fact. Hm. Tricky.
-		*/
 		String keyboard_copy = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //duplicate
 		Hashtable markers = new Hashtable();
 		for(int i = 0; i < 26; i ++) {
@@ -602,9 +418,7 @@ public class Cipher extends JFrame {
 						break;
 					}
 					if( plugboard_made.charAt(i) != keyboard_copy.charAt(i) ) {
-						//if( plugboard_made.charAt( keyboard_copy.indexOf( plugboard_made.charAt(i) ) ) != keyboard_copy.charAt( plugboard_made.indexOf( keyboard_copy.charAt(i) ) ) ) {
 						if( plugboard_made.charAt( keyboard_copy.indexOf( plugboard_made.charAt(i) ) ) != keyboard_copy.charAt(i) ) {
-							//i don't know if this is right my brain hurts
 							verified = false;
 							break;
 						}
@@ -616,6 +430,7 @@ public class Cipher extends JFrame {
 			}
 		});
 
+		//the GUI hellbeast needs taming
 		createLayout(
 			plugboard_labels[0], plugboard_labels[1], plugboard_labels[2], plugboard_labels[3], plugboard_labels[4], plugboard_labels[5], plugboard_labels[6], plugboard_labels[7], plugboard_labels[8], plugboard_labels[9], plugboard_labels[10], plugboard_labels[11], plugboard_labels[12], plugboard_labels[13], plugboard_labels[14], plugboard_labels[15], plugboard_labels[16], plugboard_labels[17], plugboard_labels[18], plugboard_labels[19], plugboard_labels[20], plugboard_labels[21], plugboard_labels[22], plugboard_labels[23], plugboard_labels[24], plugboard_labels[25], 
 			plugboard_menu[0], plugboard_menu[1], plugboard_menu[2], plugboard_menu[3], plugboard_menu[4], plugboard_menu[5], plugboard_menu[6], plugboard_menu[7], plugboard_menu[8], plugboard_menu[9], plugboard_menu[10], plugboard_menu[11], plugboard_menu[12], plugboard_menu[13], plugboard_menu[14], plugboard_menu[15], plugboard_menu[16], plugboard_menu[17], plugboard_menu[18], plugboard_menu[19], plugboard_menu[20], plugboard_menu[21], plugboard_menu[22], plugboard_menu[23], plugboard_menu[24], plugboard_menu[25], 
--- README.md
+++ README.md
@@ -1,5 +1,62 @@
-# enigma
-A separate repo for my enigma machine.
+## About this project
+This is a software implementation of the classic World War II Enigma machine. I've implemented eight wheels, three reflectors, and three wheel slots, which is far from the only configuration these machines were available in. Instead of pressing keys one at a time and having them light up bulbs, my implementation encodes a whole string of words all at once. I also allow for using the same wheel more than once, because why not.
+
+There are two versions, the first written in Java with a GUI, the second in C++ with NCurses. The Java version is very messy! But eventually I'll clean it up.
+
+## About the implementation
+Wheel specs are recorded here:
+[en.wikipedia.org](https://en.wikipedia.org/wiki/Enigma_rotor_details)
+[users.telenet.be](http://users.telenet.be/d.rijmenants/en/enigmatech.htm) (this is the page I relied on)
+[codesandciphers.org](https://www.codesandciphers.org.uk/enigma/rotorspec.htm)
+
+Actions speak louder than words, so here is a video of a paper enigma machine in action.
+[YouTube](https://www.youtube.com/watch?v=pZsuxZXN33g)
+Notice that the wheels, rotors, whatever, move BEFORE encoding the letter typed!
+
+Here also is an Arduino implementation, code included.
+[Instructables](https://www.instructables.com/id/Make-your-own-Enigma-Replica/)
+
+The Enigma machine works by sending an electrical signal...
+From the keyboard, to the plugboard, to a static wheel that connects to the moving wheels...
+From there to wheel III, to wheel II, to wheel I, to the reflector, then back again...
+Wheel I, II, III, static wheel, plugboard, then the lampboard. Result!
+Oh right and in some versions there are four wheels!
+
+So in short:
+1. Input character
+2. Shift wheels
+3. Plugboard
+4. Wheel III
+5. Wheel II
+6. Wheel I
+7. Reflector
+8. Wheel I
+9. Wheel II
+10. Wheel III
+11. Plugboard
+12. Output character!
+
+the steps to move forward (left) through the enigma machine are...
+1. Identify the entry point
+	To make it easier I'm going to transform code_letter into its un-rotated form between each wheel
+	This is derived from the position of string[0] compared to keyboard
+2. Move through the wheel
+	Going forward, this is a simple case of matching the entry character in string [0] with its counterpart in [1]
+3. Identify exit point
+	So you have the new letter from string[1]. But it needs to exit at the position of string[0]!
+	Tranform code_letter into its un-rotated form, based on that position.
+	
+the steps to move backward (right) are...
+1. Identify the entry point
+	This is the same as going forward. Transform the un-rotated letter with string[0]
+2. Move through the wheel
+	We need to find code_letter in string[1], and transform it into the matching char in string [0]
+3. Identify the exit point
+	Since we're going backwards we're already in the right position to un-rotate code_letter
+
+steps to move through the reflector and plugboard
+1. transform code_letter
+	The reflector doesn't rotate so just find code_letter in keyboard and match the position to the reflector
 
 ## Compiling
 
